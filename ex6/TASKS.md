0a727840427f0411b635ea851d84520918757364
jakob <j.grepperud@gmail.com>
2020-10-13 13:47:41 +0200

    Finish exercise 6

--- ex6/TASKS.md
+++ ex6/TASKS.md
@@ -1,1 +1,12 @@
 # TASK A
+Got output (Y)
+
+# TASK B
+Running on all cores gives much better worst case response time, because the tasks can run in parallell and doesn't have to completely wait for each other to finish. The most common response time is equal for both task A and B, but for task B where all processes run on the same core, there are some cases where to response time extends up to 25000 us.
+
+# TASK D
+
+**Without disturbance enabled - fastest reponse?** Fastest average - busy polling. Best worst-case periodic polling.
+**Less impacted by additon of disturbance?** Both seem quite equally impacted by the addition of disturbances,
+**With disturbance enabled - any have a predictable and consistent worst-case response time?** Periodic polling seems to have a quite consitent worst-case response time of around 5000 us, while busy-polling seems to exceed 25000 us at times.   
+With busy-polling the disturbances needed to have a printf in the while loop to avoid overflow. Periodic polling allowed for this printf-statement to be removed, and resulted in even better performance, with worst-case response time as low as 2500 us, with most of the response times being sub 1000 us.
--- ex6/main.c
+++ ex6/main.c
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #include "io.h"
 #include <sched.h>
+#include <time.h>
+#include "clock_gettime.h"
 
 int set_cpu(int cpu_number){
     cpu_set_t cpu;
@@ -20,7 +22,7 @@ int set_cpu(int cpu_number){
 // Note the argument and return types: 
 void* taskA(void* args)
 {   
-    //set_cpu(1);
+    set_cpu(0);
     int chan = *(int*)args;
 	while(1){
 		if(!io_read(chan)){
@@ -30,18 +32,76 @@ void* taskA(void* args)
 		}
 	}
 }
+
+void* disturbance(void* args)
+{   
+    set_cpu(0);
+    // creates a compiler level memory barrier forcing optimizer 
+    // to not re-order memory accesses across the barrier.
+    while(1){
+        asm volatile("" ::: "memory");
+        //printf("I shall not tell lies\n");
+    }
+}
+
+void* periodic_task(void* args)
+{
+    struct timespec waketime;
+    clock_gettime(CLOCK_REALTIME, &waketime);
+
+    struct timespec period = {.tv_sec = 0, .tv_nsec = 500*1000*1000};
+
+    while(1){
+
+        printf("Periodic tasking\n");
+
+        waketime = timespec_add(waketime, period);
+        clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &waketime, NULL);
+
+    }
+}
+
+void* taskD_periodic(void* args)
+{   
+    struct timespec waketime;
+    clock_gettime(CLOCK_REALTIME, &waketime);
+
+    struct timespec period = {.tv_sec = 0, .tv_nsec = 1000*1000};
+
+    set_cpu(0);
+    int chan = *(int*)args;
+	while(1){
+		if(!io_read(chan)){
+			io_write(chan, 0);
+            usleep(5);
+			io_write(chan, 1);
+		}
+        waketime = timespec_add(waketime, period);
+        clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &waketime, NULL);
+	}
+}
 int main()
 {   
-    printf("init\n");
+    //printf("init\n");
     io_init();
     pthread_t threadA;
     pthread_t threadB;
     pthread_t threadC;
-    pthread_create(&threadA, NULL, taskA, &(int){1});
-    pthread_create(&threadB, NULL, taskA, &(int){2});
-    pthread_create(&threadC, NULL, taskA, &(int){3});
-    
+    pthread_t dists[10];
+    pthread_create(&threadA, NULL, taskD_periodic, &(int){1});
+    pthread_create(&threadB, NULL, taskD_periodic, &(int){2});
+    pthread_create(&threadC, NULL, taskD_periodic, &(int){3});
+
+    for (int i = 0; i < 10; i++){
+        pthread_create(&dists[i], NULL, disturbance, NULL);
+    }
+
+    // joins won't happen
     pthread_join(threadA, NULL);
     pthread_join(threadB, NULL);
     pthread_join(threadC, NULL);
+
+    for (int i = 0; i < 10; i++){
+        pthread_join(dists[i], NULL);
+    }
 }
